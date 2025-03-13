3143748cc054127a0add2e2e3d5e5bfd22f5cf41
Afrozparveen <shaik.afrozparveen@gmail.com>
2025-03-13 12:45:38 +0530

    Update 3.2 While Loop.java

--- 3.2 While Loop.java
+++ 3.2 While Loop.java
@@ -1,3 +1,15 @@
+while Loop in Java
+A while loop is a control flow statement that repeatedly executes a block of code as long as a given condition evaluates to true. It is useful when the number of iterations is unknown in advance.
+
+Syntax of while Loop
+while (condition) {
+    // Code to execute
+}
+The condition is checked before executing the loop body.
+If the condition is true, the code inside the loop runs.
+If the condition is false, the loop stops.
+
+	
 public class hello {
 
 	public static void main(String[] args) {
