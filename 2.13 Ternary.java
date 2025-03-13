50b644b12094eca07a44c25e316dec1f1ec1280f
Afrozparveen <shaik.afrozparveen@gmail.com>
2025-03-13 11:38:22 +0530

    Update 2.13 Ternary.java

--- 2.13 Ternary.java
+++ 2.13 Ternary.java
@@ -1,3 +1,16 @@
+Ternary Operator in Java::::::::::::::::
+	
+	The ternary operator (?:) is a shorthand for if-else statements in Java. It allows for concise conditional expressions.
+
+Syntax:
+
+	variable = (condition) ? expression1 : expression2;
+
+	If the condition is true, expression1 is assigned.
+	If the condition is false, expression2 is assigned.
+	
+EXAMPLE:::::::::::::::::::::::::
+
 public class hello {
 
 	public static void main(String[] args) {
