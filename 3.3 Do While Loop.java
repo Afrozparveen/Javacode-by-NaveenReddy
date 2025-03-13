87bd4a0317b283149bbf23456576a9e4ece84df3
Afrozparveen <shaik.afrozparveen@gmail.com>
2025-03-13 12:46:57 +0530

    Update 3.3 Do While Loop.java

--- 3.3 Do While Loop.java
+++ 3.3 Do While Loop.java
@@ -1,3 +1,17 @@
+do-while Loop in Java::::::::::::
+ 	A do-while loop is a control flow statement that executes a block of code at least once, and then repeats the execution as long as a specified condition remains true.
+
+Syntax of do-while Loop
+	
+do {
+    // Code to execute
+} while (condition);
+
+The loop body executes first before checking the condition.
+If the condition is true, the loop repeats.
+If the condition is false, the loop terminates.
+
+	
 public class hello {
 
 	public static void main(String[] args) {
