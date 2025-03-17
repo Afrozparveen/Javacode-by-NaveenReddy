1abdc0a5c1e4a1d3875ffc8c9280d58e4f5bf095
Afrozparveen <shaik.afrozparveen@gmail.com>
2025-03-17 12:11:12 +0530

    Update 6.1 Need Of an Array.java

--- 6.1 Need Of an Array.java
+++ 6.1 Need Of an Array.java
@@ -1,3 +1,21 @@
+Why Do We Need an Array in Java?
+  An array is a fundamental data structure in Java that allows us to store multiple values of the same type under a single variable name. It is needed for efficient data storage, organization, and retrieval.
+
+1. Storing Multiple Values
+  Without arrays, we would have to create multiple variables for storing multiple values of the same type.
+
+Example (Without Arrays):
+
+int num1 = 10;
+int num2 = 20;
+int num3 = 30;
+If we have hundreds of values, this approach is impractical.
+
+Example (With Arrays):
+
+int[] numbers = {10, 20, 30, 40, 50};
+
+EXAMPLE:::::::::::::::::::::::::
 int i=5;
 int j=6;
 int k=7;
